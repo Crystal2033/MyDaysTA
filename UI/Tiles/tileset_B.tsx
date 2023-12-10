d91b5fcffba7bbe6a2e0e11b883a659a67d5dc6d
Pavel <kulikov-pavlik@bk.ru>
2023-12-11 00:34:19 +0300

    Added tiles

---
+++ UI/Tiles/tileset_B.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Test16x16" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
+ <image source="Interier/rpgmaker/mvmz/tileset_B.png" width="768" height="768"/>
+</tileset>
