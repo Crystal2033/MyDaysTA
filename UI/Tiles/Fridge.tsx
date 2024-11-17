41e7ee0da334ab39fd367a359e1bf0a6615a7c69
Pavel <kulikov-pavlik@bk.ru>
2024-11-17 16:35:23 +0300

    Added needed files

---
+++ UI/Tiles/Fridge.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Fridge" tilewidth="16" tileheight="16" tilecount="2" columns="1">
+ <image source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites coloured outlines/Slice 181.png" trans="ff00ff" width="16" height="32"/>
+</tileset>
---
+++ UI/Tiles/furniture_concrete.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="chair_kitchen" tilewidth="32" tileheight="32" tilecount="19" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="13" height="16" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 101.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="15" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 250.png"/>
+ </tile>
+ <tile id="3">
+  <image width="10" height="13" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 459.png"/>
+ </tile>
+ <tile id="4">
+  <image width="11" height="15" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 7.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 10.png"/>
+ </tile>
+ <tile id="7">
+  <image width="8" height="12" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 14.png"/>
+ </tile>
+ <tile id="8">
+  <image width="8" height="14" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 15.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 31.png"/>
+ </tile>
+ <tile id="10">
+  <image width="15" height="11" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 40.png"/>
+ </tile>
+ <tile id="11">
+  <image width="11" height="16" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 183.png"/>
+ </tile>
+ <tile id="12">
+  <image width="13" height="16" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 220.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 276.png"/>
+ </tile>
+ <tile id="14">
+  <image width="10" height="11" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 302.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 362.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 367.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 372.png"/>
+ </tile>
+ <tile id="18">
+  <image width="15" height="24" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 467.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="30" source="../../TilesSprites/FurniturePack/sierrassets furniture pack/furniture/individual sprites grey outline/part-Slice 468.png"/>
+ </tile>
+</tileset>
